a9df25cbc7998c50aec8b0744b8581020fa3c540
guptavaibhav.05086@gmail.com <guptavaibhav.05086@gmail>
2019-02-17 15:46:55 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'session2';
+  title = 'Session2';
+  myHero = 'Windstorm';
+  heroImageUrl = 'assets/images/hero.png';
+  isUnchanged = true;
+  isSpecial = false;
+  isMouseIn = false;
+  value:string ="";
+  bindValue:string = "2 way Binding";
+  ngBinding="NgModelBinding";
+
+  getVal(){
+    return 5;
+  }
+
+
+  changeTitle(){
+    this.title = 'Angular Session 2';
+    this.bindValue = 'value flow from component to View';
+
+
+  }
+
+  buttonClickEvent(){
+    alert('Button is Clicked');
+  }
+  onKeyBoarEvent($event : KeyboardEvent){
+    let msg = "User Press the Key " + $event.key;
+    alert(msg);
+    this.value += (<HTMLInputElement>event.target).value + ' | ';
+     
+
+  }
+  mouseEnterEvents(){
+    this.isMouseIn = true;
+
+  }
+  mouseLeaveEvents(){
+    this.isMouseIn = false;
+  }
 }
